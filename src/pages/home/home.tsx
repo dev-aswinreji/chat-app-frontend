29424b8ec5232b4621052a7019af59824eec8ce8
Aswin Reji <aswinreji.004@gmail.com>
2025-03-14 22:53:43 +0000

    feat: we fix the issue regarding refreshing the page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,31 +4,29 @@ import useConversation from "../../zustand/useConversation";
 import { useEffect } from "react";
 
 const Home = () => {
-    const { setSelectedConversation, selectedConversation } = useConversation();
+  const { setSelectedConversation, selectedConversation } = useConversation();
 
-    useEffect(() => {
-        return () => {
-            setSelectedConversation(null);
-        }
-    }, [setSelectedConversation]);
+  useEffect(() => {
+    setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
-    return (
-        <div className="w-full h-full rounded-lg overflow-hidden">
-            {/* Mobile view */}
-            <div className="md:hidden h-full w-full">
-                {selectedConversation ? (
-                    <MessageContainer mobileView={true} />
-                ) : (
-                    <Sidebar mobileView={true} />
-                )}
-            </div>
-            {/* Desktop view */}
-            <div className="hidden md:flex h-full w-full">
-                <Sidebar />
-                {selectedConversation && <MessageContainer />}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full h-full rounded-lg overflow-hidden">
+      {/* Mobile view */}
+      <div className="md:hidden h-full w-full">
+        {selectedConversation ? (
+          <MessageContainer mobileView={true} />
+        ) : (
+          <Sidebar mobileView={true} />
+        )}
+      </div>
+      {/* Desktop view */}
+      <div className="hidden md:flex h-full w-full">
+        <Sidebar />
+        {selectedConversation && <MessageContainer />}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
