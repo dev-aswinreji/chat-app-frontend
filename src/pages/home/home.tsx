91cfb3e694f9115400dd79680ca93055546f2aba
Aswin Reji <aswinreji.004@gmail.com>
2025-03-14 22:48:06 +0000

    feat: we fix the issue regarding refreshing the page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,28 +1,32 @@
 import Sidebar from "../../components/sidebar/sidebar";
 import MessageContainer from "../../components/messages/messageContainer";
 import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
 
 const Home = () => {
-  const { selectedConversation } = useConversation();
+    const { setSelectedConversation, selectedConversation } = useConversation();
 
-  return (
-    <div className="w-full h-full rounded-lg overflow-hidden">
-      {/* Mobile view: show Sidebar if no conversation is selected, otherwise show chat view */}
-      <div className="md:hidden h-full w-full">
-        {selectedConversation ? (
-          <MessageContainer mobileView={true} />
-        ) : (
-          <Sidebar mobileView={true} />
-        )}
-      </div>
+    useEffect(() => {
+        setSelectedConversation(null);
+    }, [setSelectedConversation]);
 
-      {/* Desktop view: always show Sidebar, and show MessageContainer only when a conversation is selected */}
-      <div className="hidden md:flex h-full w-full">
-        <Sidebar />
-        {selectedConversation && <MessageContainer />}
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full h-full rounded-lg overflow-hidden">
+            {/* Mobile view */}
+            <div className="md:hidden h-full w-full">
+                {selectedConversation ? (
+                    <MessageContainer mobileView={true} />
+                ) : (
+                    <Sidebar mobileView={true} />
+                )}
+            </div>
+            {/* Desktop view */}
+            <div className="hidden md:flex h-full w-full">
+                <Sidebar />
+                {selectedConversation && <MessageContainer />}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
